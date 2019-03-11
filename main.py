a7aaa4d14ceca166d92bbddef0fe4bf700f1fd48
LuisErnestoColchado <agoraphobiacroz@gmail.com>
2019-03-10 21:40:28 -0500

    get mean features

--- main.py
+++ main.py
@@ -13,10 +13,31 @@ fileTrain = "data/train/All_QA_Pairs_train.txt"
 fileValidation = "data/validation/All_QA_Pairs_val.txt"
 trainX, trainY = readFile(fileTrain)
 valX, valY = readFile(fileValidation)
+X = np.array(trainX)
 
 modelW2VTrainX = wordEmbeddings(trainX)
 modelW2VTrainY = wordEmbeddings(trainY)
 modelW2VValX = wordEmbeddings(valX)
 modelW2VValY = wordEmbeddings(valY)
 
+
+print(type(X))
+
+print(X)
+mean = getMean(X)
+print(mean)
+#modelW2VTrainX.build_vocab(trainX)
+#modelW2VTrainX.train(trainX,total_examples=len(trainX),epochs=10)
+
+#print(modelW2VTrainX.most_similar('good'))
+
+#learningRate = np.power(10.0,-2.0)
+#trainingSteps = 20000
+#batchSize = 1000
+#displayStep = 1000
+#numInput = 119
+#timeStep = 1
+#numUnits = 119
+#numClasses = 1
+#nunLayers = 8
 
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from gensim.models import word2vec
+import numpy as np
 
 def readFile(nameFile):
     sentencesX = []
@@ -13,3 +14,39 @@ def readFile(nameFile):
 def wordEmbeddings(sentences):
     model = word2vec.Word2Vec(sentences, size=200)
     return model 
+
+def getMean(xTrain):
+    numWords = []
+    for x in xTrain:
+        count = len(x)
+        numWords.append(count)
+    mean = np.mean(numWords)
+    return int(mean)
+
+def featureVecMethod(words, model, num_features):
+
+    featureVec = np.zeros(num_features,dtype="float32")
+    nwords = 0
+
+    index2word_set = set(model.wv.index2word)
+    
+    for word in  words:
+        if word in index2word_set:
+            nwords = nwords + 1
+            featureVec = np.add(featureVec,model[word])
+
+    featureVec = np.divide(featureVec, nwords)
+    return featureVec
+
+def getAvgFeatureVecs(reviews, model, num_features):
+    counter = 0
+    reviewFeatureVecs = np.zeros((len(reviews),num_features),dtype="float32")
+    for review in reviews:
+
+        if counter%1000 == 0:
+            print("Review %d of %d"%(counter,len(reviews)))
+            
+        reviewFeatureVecs[counter] = featureVecMethod(review, model, num_features)
+        counter = counter+1
+        
+    return reviewFeatureVecs
